4307e069c63672ca779c986ad5f4005cc5b090c4
pezosanta <pezosanta@gmail.com>
2020-05-24 20:56:12 +0000

    separated model initialization and prediction

--- predict.py
+++ predict.py
@@ -4,12 +4,8 @@ import numpy as np
 from dataset import TSDataset
 from vgg import vgg16_bn
 
-def predictTS(image, convertToRGB):
-    if convertToRGB == True:
-        image                           = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    tensorImage                         = torch.unsqueeze(torch.from_numpy(image / 255.0).float().to(device = 'cuda:0').permute(2, 0, 1), 0)
-
+def loadModelParams():
     checkpoint_path                     = './vgg16_pretrained-epoch2.pth'
     checkpoint                          = torch.load(checkpoint_path)
 
@@ -18,12 +14,21 @@ def predictTS(image, convertToRGB):
     model                               = vgg16_bn(pretrained = False, num_classes = 55)
 
     model.load_state_dict(state_dict = model_state_dict, strict = True)
-    model.to(device = 'cuda:0')
+    model.to(device = 'cuda' if torch.cuda.is_available() else 'cpu')
     model.eval()
 
-    outs = model(tensorImage)
+    return model
 
-    prediction = torch.argmax(outs).item()
+def predictTS(image, model, convertToRGB):
+    if convertToRGB == True:
+        image                           = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    tensorImage                         = torch.unsqueeze(torch.from_numpy(image / 255.0).float().to(device = 'cuda' if torch.cuda.is_available() else 'cpu').permute(2, 0, 1), 0)
+
+    with torch.no_grad():
+        outs = model(tensorImage)
+
+        prediction = torch.argmax(outs).item()
 
     return prediction
 
